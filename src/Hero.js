a3b86a609e9fd1dc7f53382bb8bca1a17309ee12
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-07-22 22:22:37 +0200

    Function hero added

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Hero.css";
 
-export const Hero = () => {
-  return <div className="hero">Hero</div>;
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <h1>YOUR RELIABLE PEST CONTROL SERVICE IN HUNGARY</h1>
+      </div>
+    </div>
+  );
 };
+export default Hero;
